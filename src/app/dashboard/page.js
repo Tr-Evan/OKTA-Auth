538b30144bca279919ac893c25318a4c2921ce49
Tr-Evan <contact.evantroget@gmail.com>
2024-07-15 16:21:23 +0200

    login okta & middleware OK

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,23 +1,5 @@
 // src/app/dashboard/page.js
 
-import { getSession } from '@auth0/nextjs-auth0';
-
-export async function getServerSideProps(context) {
-  const session = getSession(context.req, context.res);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/api/auth/login',
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {}, // passe des props si nécessaire
-  };
-}
 
 export default function Dashboard() {
   return (
